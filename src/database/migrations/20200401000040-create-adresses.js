772b7ca684f29f12336efa101e2a1278b7711b67
LucasFDutra <lucasfelipedutra@gamil.com>
2020-03-31 22:25:47 -0300

    create addresses table and addresses model and controller

---
+++ src/database/migrations/20200401000040-create-adresses.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('addresses', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    user_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: { model: 'users', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    },
+    zipcode: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    street: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    number: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('users'),
+};
